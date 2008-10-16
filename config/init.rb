f224bdd5380e34db89b35291336dd2fe8033cf9f
rick <technoweenie@gmail.com>
2008-10-16 15:57:19 -0700

    allow setting of mapping domains

--- config/init.rb
+++ config/init.rb
@@ -16,9 +16,7 @@ Merb::Config.use do |c|
 end
  
 Merb::BootLoader.before_app_loads do
-  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
 end
  
 Merb::BootLoader.after_app_loads do
-  # This will get executed after your app's classes have been loaded.
 end
