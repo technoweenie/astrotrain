3875886e071d253927f8af7082f8d6593360cd4c
John Nunemaker <nunemaker@gmail.com>
2008-10-24 11:03:43 -0400

    Added some styles, default route and basic add/edit works.

--- config/router.rb
+++ config/router.rb
@@ -34,5 +34,5 @@ Merb::Router.prepare do
   #default_routes
   
   # Change this for your home page to be available at /
-  # match('/').to(:controller => 'whatever', :action =>'index')
+  match('/').to(:controller => 'mappings', :action =>'index')
 end
