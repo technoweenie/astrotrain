cde340c7176b5f732cefe4cc9c49cf1379264301
rick <technoweenie@technoweenie.local>
2008-10-25 10:47:30 -0700

    bump deps for merb 1.0 rc2

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,11 +1,11 @@
-dependency "merb-action-args", "0.9.9"   # Provides support for querystring arguments to be passed in to controller actions
-dependency "merb-assets", "0.9.9"        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
-dependency "merb-cache", "0.9.9"         # Provides your application with caching functions 
-dependency "merb-helpers", "0.9.9"       # Provides the form, date/time, and other helpers
-dependency "merb-mailer", "0.9.9"        # Integrates mail support via Merb Mailer
-dependency "merb-slices", "0.9.9"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
-dependency "merb-auth", "0.9.9"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
-dependency "merb-param-protection", "0.9.9"
+dependency "merb-action-args", "0.9.10"   # Provides support for querystring arguments to be passed in to controller actions
+dependency "merb-assets", "0.9.10"        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
+dependency "merb-cache", "0.9.10"         # Provides your application with caching functions 
+dependency "merb-helpers", "0.9.10"       # Provides the form, date/time, and other helpers
+dependency "merb-mailer", "0.9.10"        # Integrates mail support via Merb Mailer
+dependency "merb-slices", "0.9.10"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
+dependency "merb-auth", "0.9.10"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
+dependency "merb-param-protection", "0.9.10"
  
 dependency "dm-core", "0.9.6"         # The datamapper ORM
 dependency "dm-aggregates", "0.9.6"   # Provides your DM models with count, sum, avg, min, max, etc.
