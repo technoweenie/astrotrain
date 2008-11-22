b85a953e6b679c1bdc393afba9bd0e42723e3e0c
rick <technoweenie@gmail.com>
2008-11-21 16:06:47 -0800

    update to merb 1.0.1 and dm 0.9.7

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,4 +1,4 @@
-ver = '0.9.12'
+ver = '1.0.1'
 dependency "merb-action-args", ver   # Provides support for querystring arguments to be passed in to controller actions
 dependency "merb-assets", ver        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
 dependency "merb-cache", ver         # Provides your application with caching functions 
@@ -8,12 +8,13 @@ dependency "merb-slices", ver        # Provides a mechanism for letting plugins 
 dependency "merb-auth", ver          # An authentication slice (Merb's equivalent to Rails' restful authentication)
 dependency "merb-param-protection", ver
  
-dependency "dm-core", "0.9.6"         # The datamapper ORM
-dependency "dm-aggregates", "0.9.6"   # Provides your DM models with count, sum, avg, min, max, etc.
-dependency "dm-migrations", "0.9.6"   # Make incremental changes to your database.
-dependency "dm-timestamps", "0.9.6"   # Automatically populate created_at, created_on, etc. when those properties are present.
-dependency "dm-types", "0.9.6"        # Provides additional types, including csv, json, yaml.
-dependency "dm-validations", "0.9.6"  # Validation framework
+dm_ver = "0.9.7"
+dependency "dm-core", dm_ver         # The datamapper ORM
+dependency "dm-aggregates", dm_ver   # Provides your DM models with count, sum, avg, min, max, etc.
+dependency "dm-migrations", dm_ver   # Make incremental changes to your database.
+dependency "dm-timestamps", dm_ver   # Automatically populate created_at, created_on, etc. when those properties are present.
+dependency "dm-types", dm_ver        # Provides additional types, including csv, json, yaml.
+dependency "dm-validations", dm_ver  # Validation framework
 
 dependency "tmail", "1.2.3.1"
 dependency "curb", "0.1.4"
