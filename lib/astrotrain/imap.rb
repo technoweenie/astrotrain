4ef123c4af2891692dd82eb60484231e37f93400
rick <technoweenie@gmail.com>
2009-02-16 22:24:51 -0800

    add imap connection stuff

---
+++ lib/astrotrain/imap.rb
@@ -0,0 +1,203 @@
+require 'net/imap'
+require 'stringio'
+require 'ostruct'
+
+module Astrotrain
+  class Imap
+    class << self
+      attr_accessor :connection_class
+    end
+    self.connection_class = Net::IMAP
+
+    attr_reader :host
+    attr_reader :options
+
+    # :host, :port, :use_ssl, :user, :password
+    def initialize(host, options = {})
+      @host    = host
+      @options = options
+    end
+
+    def connection
+      if @connection.nil? || @connection.disconnected?
+        @connection = self.class.connection_class.new(@host, @options[:port], @options[:use_ssl])
+        @connection.login @options[:user], @options[:password]
+      end
+      @connection
+    end
+
+    def close
+      if @connection && !@connection.disconnected?
+        @connection.logout
+        @connection.disconnect
+      end
+    end
+
+    def mailboxes(refname = '', mailbox = '*')
+      mailboxes = []
+      connection.list(refname, mailbox).each do |m|
+        mailboxes << Mailbox.new(self, m.name)
+      end
+      mailboxes
+    end
+
+    def ==(other)
+      other.class == self.class && other.host == @host && other.options == @options
+    end
+
+    def inspect
+      %(#<Astrotrain::Imap @host=#{@host}#{":#{@options[:port]}" if @options[:port]}>)
+    end
+
+    class Mailbox
+      attr_reader :name
+      attr_reader :connection
+
+      def initialize(imap, name)
+        @connection = imap.connection
+        @name       = name
+      end
+
+      def with
+        @connection.select @name
+        retval = yield
+        @connection.close
+        retval
+      end
+
+      def search(q = 'ALL', options = {})
+        options[:limit] ||= 15
+        search_for_ids(q.to_s.split(" ")) do |ids|
+          ids = ids[0..options[:limit]-1] if ids.size > options[:limit]
+          @connection.fetch(ids, 'RFC822').map! { |m| Message.new(m.seqno, m.attr['RFC822']) }
+        end
+      end
+
+      def move(messages, destination)
+        return if messages.empty?
+        messages = messages.map { |m| m.number } if messages.first.respond_to?(:number)
+        @connection.copy messages, destination.name
+        destroy_messages messages
+      end
+
+      def destroy
+        @connection.delete @name
+      end
+
+      def destroy_messages(messages)
+        return if messages.empty?
+        messages = messages.map { |m| m.number } if messages.first.respond_to?(:number)
+        @connection.store messages, "+FLAGS", [:Deleted]
+      end
+
+      def ==(other)
+        other.class == self.class && @connection == other.connection && @name == other.name
+      end
+
+      def inspect
+        %(#<Astrotrain::Imap::Mailbox @name=#{@name}>)
+      end
+
+    protected
+      def search_for_ids(*args)
+        args = %w(ALL) if args.empty?
+        with do
+          ids = @connection.search(*args)
+          block_given? ? yield(ids) : ids
+        end
+      end
+    end
+
+    class Message
+      attr_reader :number
+      attr_reader :raw
+
+      def initialize(number, raw)
+        @number = number
+        @raw    = raw
+      end
+
+      def ==(other)
+        @number == other.number
+      end
+
+      def inspect
+        %(#<Astrotrain::Imap::Message @number=#{@number}>)
+      end
+    end
+
+    class MockConnection
+      attr_reader :host
+      attr_reader :port
+      attr_reader :use_ssl
+      attr_reader :trace
+
+      def initialize(host, port, use_ssl)
+        @host    = host
+        @port    = port
+        @use_ssl = use_ssl
+        clear
+        close
+      end
+
+      def login(*args)
+        write_trace :login, *args
+      end
+
+      def list(*args)
+        write_trace :list, *args
+      end
+
+      def select(*args)
+        write_trace :select, *args
+      end
+
+      def delete(*args)
+        write_trace :delete, *args
+      end
+
+      def store(*args)
+        write_trace :store, *args
+      end
+
+      def copy(*args)
+        write_trace :copy, *args
+      end
+
+      def search(*args)
+        write_trace :search, *args
+      end
+
+      def fetch(*args)
+        write_trace :fetch, *args
+      end
+
+      def disconnected?
+        @disconnected == true
+      end
+
+      def close
+        @disconnected = true
+      end
+
+      # TESTER API
+      def mock(method, *args, &block)
+        @mocks[[method, args]] = block
+      end
+
+      def clear
+        @mocks = {}
+        @trace = []
+      end
+
+    protected
+      def write_trace(method, *args)
+        @disconnected = false
+        @trace << "#{method}(#{args.inspect})"
+        if mock = @mocks[[method.to_sym, args]]
+          mock.call
+        end
+      end
+    end
+  end
+end
---
+++ spec/models/astrotrain/imap_spec.rb
@@ -0,0 +1,75 @@
+require File.join( File.dirname(__FILE__), '..', '..', "spec_helper" )
+require 'astrotrain/imap'
+
+Astrotrain::Imap.connection_class = Astrotrain::Imap::MockConnection
+
+describe "Astrotrain" do
+  before do
+    @imap = Astrotrain::Imap.new('host', :port => 1, :use_ssl => true, :user => 'ricky', :password => 'bobby')
+    @conn = @imap.connection
+    @mbox = Astrotrain::Imap::Mailbox.new(@imap, "foo")
+  end
+
+  describe "Imap" do
+    it "logs into imap server" do
+      @imap.connection
+      @conn.trace.should == [%(login(["ricky", "bobby"]))]
+    end
+
+    it "finds mailboxes" do
+      @conn.mock(:list, '', '*') { [OpenStruct.new(:name => 'foo'), OpenStruct.new(:name => 'bar')] }
+      @imap.mailboxes.should == [Astrotrain::Imap::Mailbox.new(@imap, 'foo'), Astrotrain::Imap::Mailbox.new(@imap, 'bar')]
+    end
+
+    it "opens connection" do
+      @imap.connection.should_not be_disconnected
+    end
+
+    it "closes connection" do
+      @imap.connection.close
+      @conn.should be_disconnected
+    end
+  end
+
+  describe "Imap::Mailbox" do
+    it "performs operations inside the mailbox" do
+      @mbox.with { 1.should == 1 }
+      @conn.trace.last.should == %(select([#{@mbox.name.inspect}]))
+    end
+
+    it "searches mailbox" do
+      @conn.mock(:search, ["foo", "bar"])    { [2, 1] }
+      @conn.mock(:fetch, [2, 1], 'RFC822') { [OpenStruct.new(:seqno => 2, :attr => {}), OpenStruct.new(:seqno => 1, :attr => {})] }
+      @mbox.search("foo bar").should == [Astrotrain::Imap::Message.new(2, nil), Astrotrain::Imap::Message.new(1, nil)]
+    end
+
+    it "destroys mailbox" do
+      @mbox.destroy
+      @conn.trace.last.should == %(delete([#{@mbox.name.inspect}]))
+    end
+
+    it "moves given message ids" do
+      @other = Astrotrain::Imap::Mailbox.new(@imap, "bar")
+      @mbox.move [1, 2], @other
+      @conn.trace.pop.should == %(store([[1, 2], "+FLAGS", [:Deleted]]))
+      @conn.trace.pop.should == %(copy([[1, 2], "bar"]))
+    end
+
+    it "moves given messages" do
+      @other = Astrotrain::Imap::Mailbox.new(@imap, "bar")
+      @mbox.move [OpenStruct.new(:number => 1), OpenStruct.new(:number => 2)], @other
+      @conn.trace.pop.should == %(store([[1, 2], "+FLAGS", [:Deleted]]))
+      @conn.trace.pop.should == %(copy([[1, 2], "bar"]))
+    end
+
+    it "destroys given message_ids" do
+      @mbox.destroy_messages [1, 2]
+      @conn.trace.last.should == %(store([[1, 2], "+FLAGS", [:Deleted]]))
+    end
+
+    it "destroys given messages" do
+      @mbox.destroy_messages [OpenStruct.new(:number => 1), OpenStruct.new(:number => 2)]
+      @conn.trace.last.should == %(store([[1, 2], "+FLAGS", [:Deleted]]))
+    end
+  end
+end
